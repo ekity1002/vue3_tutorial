2d2b5a78e65f6ec9a84da3892bd3a538191e4fbf
ekity1002 <dreamguidance1@gmail.com>
2022-07-21 23:41:10 +0900

    chapter3 finished.

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = Vue.createApp({
     // template: '<h2>template</h2>' //template を埋め込む
     data() {
         return {
+            url: "https://maku.blog/p/qcoz9ju/",
             showBooks: true,
             title: 'The book title is vue.',
             age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                {title: 'name of the wind', author: 'hoge', img: 'assets/1.jpg', isFav: true},
+                {title: 'bafew', author: 'piyo' ,img: 'assets/2.jpg', isFav: false},
+                {title: 'asdcccc', author: 'fuga', img: 'assets/1.jpg', isFav: true},
+            ]
         }
     },
     methods: {
@@ -15,7 +23,25 @@ const app = Vue.createApp({
         },
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
+        },
+        handleEvent(e, arg) {
+            console.log(e);
+            console.log(e.type);
+            console.log(arg);
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
+    },
+    computed: {
+        filterdBooks() {
+            // books から isFavが false のものを除いて返却する
+            return this.books.filter((book) => book.isFav) 
+        },
     }
 }
 ) //Vue appをつくる
